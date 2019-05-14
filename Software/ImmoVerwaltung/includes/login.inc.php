92916ad2b8e36c5232d6cb2ca1a4dea5a1fd4bbf
eduard <eduard@eduard>
2019-05-14 14:07:56 +0200

    changes to login and signup

--- Software/ImmoVerwaltung/includes/login.inc.php
+++ Software/ImmoVerwaltung/includes/login.inc.php
@@ -1,1 +1,11 @@
 <?php
+
+if(isset($_POST['login-submit'])){
+    
+    require 'databasehandler.inc.php';
+    
+    
+}else{
+    header("Location: ../index.php");
+    exit();
+}
--- Software/ImmoVerwaltung/includes/signup.inc.php
+++ Software/ImmoVerwaltung/includes/signup.inc.php
@@ -2,11 +2,26 @@
 
 if(isset($_POST['signup-submit'])){
     
+    //require datenbank verbindung
     require 'databasehandler.inc.php';
     
+    //POST methoden um variablen aus Form aus signup.php zu erhalten
     $username = $_POST['id'];
     $email = $_POST['mail'];
     $password = $_POST['pwd'];
     $password_repeat = $_POST['pwd-repeat'];
+    
+    
+    //gucken, ob variablen leer sind, wenn ja dann emptyfield error
+    if(empty($username) || empty($email) || empty($password) || empty($password_repeat)){
+        header("location: ../signup.php?error=emptyfields&id=".$username."&mail".$email);
+        exit();
+    }
+    
+    //Filter um zu checken ob email gueltig ist
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        header("location: ../signup.php?error=invalidemail&id=".$username);
+        exit();
+    }
     
 }
