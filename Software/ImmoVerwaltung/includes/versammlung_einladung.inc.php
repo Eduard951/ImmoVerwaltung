aaefab1335bc47a68cddcfade4a5d5afd4c31052
eduard <eduard@eduard>
2019-05-23 15:04:41 +0200

    Vollmacht Formular + Images ordner für Bilder

--- Software/ImmoVerwaltung/includes/versammlung_einladung.inc.php
+++ Software/ImmoVerwaltung/includes/versammlung_einladung.inc.php
@@ -13,21 +13,38 @@ if(isset($_POST['versammlung_einladung_submit'])){
     $text = $_POST['text'];
     $tagesordnung= $_POST['tagesordnung'];
     $punkte = explode(",", $tagesordnung);
+    $datum_update = explode("-",$datum);
     
     //gucken, ob variablen leer sind, wenn ja dann emptyfield error
     if(!empty($datum)&& !empty($uhrzeit)&& !empty($ort) && !empty($text)){
+        
+        $sql = "SELECT * FROM benutzer WHERE BenutzerID=?;";
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../index.php?error=sqlerror");
+            exit();
+        }else{
+            $id = $_SESSION['sessionid'];
+            mysqli_stmt_bind_param($stmt, "i", $id);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+            if($row=mysqli_fetch_assoc($result)){
+                
+                $vorname = $row['Vorname'];
+                $nachname = $row['Name'];
                 
                 
                 $pdf = new FPDF();
                 $pdf->AddPage();
                 $pdf->SetFont("Arial","",12);
-                $pdf->Cell(10,10,"",0,0);
+                $pdf->Cell(10,10,"Hausverwaltung ".$nachname,0,0);
                 $pdf->Ln(5);
-                $pdf->Cell(10,10,"",0,0);
+                $pdf->Cell(10,10,"Telefon: 000",0,0);
                 $pdf->Ln(5);
-                $pdf->Cell(10,10,"Strasse Versender",0,0);
+                $pdf->Cell(10,10,"Strasse Verwaltung",0,0);
+                $pdf->Image("../images/icon.png",150,10,50,50);
                 $pdf->Ln(5);
-                $pdf->Cell(10,10,"Stadt Versender",0,0);
+                $pdf->Cell(10,10,"Stadt Verwaltung",0,0);
                 
                 $pdf->Ln(10);
                 $pdf->SetFont("Arial","",12);
@@ -43,8 +60,8 @@ if(isset($_POST['versammlung_einladung_submit'])){
                 $pdf->Cell(10,10,"________________________________________________________________________________",0,0);
                 
                 $pdf->Ln(10);
-                $pdf->SetFont("Arial","B",14);
-                $pdf->MultiCell(0, 10,"Einladung zur Eigentuemerversammlung am ".$datum." der Wohnungseigentuemergemeinschaft Objektadresse");
+                $pdf->SetFont("Arial","B",13);
+                $pdf->MultiCell(0, 10,"Einladung zur Eigentuemerversammlung am ".$datum_update[2].".".$datum_update[1].".".$datum_update[0]." der Wohnungseigentuemergemeinschaft Objektadresse");
                 $pdf->Ln(5);
                 $pdf->SetFont("Arial","",12);
                 $pdf->Cell(10, 10, "Sehr geehrte/r Frau/Herr ,");
@@ -52,7 +69,7 @@ if(isset($_POST['versammlung_einladung_submit'])){
                 $pdf->SetFont("Arial","",12);
                 $pdf->MultiCell(0, 5,"zur diesjaehrigen ordentlichen Wohnungseigentuemerversammlung der Wohnungseigentuemergemeinschaft Objektadresse laden wir sie hiermit ein.");
                 $pdf->Ln(5);
-                $pdf->Cell(10, 10, "Datum: ".$datum);
+                $pdf->Cell(10, 10, "Datum: ".$datum_update[2].".".$datum_update[1].".".$datum_update[0]);
                 $pdf->Ln(5);
                 $pdf->Cell(10, 10, "Zeit: ".$uhrzeit." Uhr");
                 $pdf->Ln(5);
@@ -77,15 +94,18 @@ if(isset($_POST['versammlung_einladung_submit'])){
                 $pdf->Ln(15);
                 $pdf->Cell(10, 10, "Anlagen: ");
                 $pdf->Output();
+                
+                
     }else{
         header("Location: ../versammlung_einladung.php?error=emptyfields");
         exit();
     }
-            
+        }
                 
 }else{
 header("Location: ../versammlung_einladung.php?error=error");
 exit();
 }
 
+}
 
---
+++ Software/ImmoVerwaltung/includes/vollmacht.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+
+require 'dbh.inc.php';
+require "../lib/fpdf181/fpdf.php";
+
+$sql = "SELECT * FROM benutzer WHERE BenutzerID=?;";
+$stmt = mysqli_stmt_init($conn);
+if(!mysqli_stmt_prepare($stmt, $sql)){
+    header("Location: ../index.php?error=sqlerror");
+    exit();
+}else{
+    $id = $_SESSION['sessionid'];
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if($row=mysqli_fetch_assoc($result)){
+        
+        $vorname = $row['Vorname'];
+        $nachname = $row['Name'];
+        
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetFont("Arial","",12);
+        $pdf->Cell(10,10,"An: ");
+        $pdf->Ln(10);
+        $pdf->Cell(10,10,"Hausverwaltung ".$nachname,0,0);
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Telefon: 000",0,0);
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Strasse Verwaltung",0,0);
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Stadt Verwaltung",0,0);
+        $pdf->Ln(10);
+        $pdf->SetFont("Arial","B",12);
+        $pdf->MultiCell(0,10,"Vollmacht zur Eigentuemerversammlung 2019 
+        der 
+        Wohnungseigentuemergemeinschaft Musterstr. 78, 38294 Musterstadt",0,"C");
+        $pdf->SetFont("Arial","",12);
+        $pdf->Ln(10);
+        $pdf->Cell(10,10,"Hiermit uebertrage(n) ich/wir:");
+        $pdf->Ln(10);
+        $pdf->Cell(10,10,$vorname." ".$nachname);
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Strasse");
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Plz und Ort");
+        
+        $pdf->Ln(15);
+        $pdf->SetFont("Arial","",11);
+        $pdf->Cell(10,10,"Meine/Unsere Vollmacht zur Versammlung am 'Datum'");
+        $pdf->Ln(10);
+        $pdf->Cell(10,10,"o an Herrn/Frau_______________________________");
+        $pdf->Ln(6);
+        $pdf->Cell(10,10,"o an den Versammlungsleiter");
+        $pdf->Ln(15);
+        $pdf->MultiCell(0,5,"o Ich/Wir ermaechtige(n) die/den Bevollmaechtigte(n) saemtliche Abstimmungen vorbehaltlos nach ihrem/seinem Ermessen vorzunehmen.");
+        
+        $pdf->Ln(10);
+        $pdf->Cell(10,10,"Diese Vollmacht ist nicht uebertragbar, eine Unterbevollmaechtigung ist erlaubt.");
+        $pdf->Ln(8);
+        $pdf->MultiCell(0, 10,"Bitte beachten Sie auch Vorgaben zur Bevollmaechtigung, die ggf. in Teilungserklaerungen notiert sind.");
+        
+        $pdf->Ln(15);
+        $pdf->Cell(10,10,"________________________");
+        $pdf->Ln(5);
+        $pdf->Cell(10,10,"Ort, Datum");
+        
+        $pdf->Output();
+        
+    }
+}
