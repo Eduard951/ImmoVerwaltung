db51f6cb4daadf807d11f43511eb2558fabfde80
Eduard Scherf <50400099+Eduard951@users.noreply.github.com>
2019-06-01 17:16:42 +0200

    Update pdf_header.php

--- Software/ImmoVerwaltung/includes/pdf_templates/basic_pdf/pdf_header.php
+++ Software/ImmoVerwaltung/includes/pdf_templates/basic_pdf/pdf_header.php
@@ -5,9 +5,9 @@ $pdf->Cell(10,10,$nachname,0,0);
 $pdf->Ln(5);
 $pdf->Cell(10,10,$vorname,0,0);
 $pdf->Ln(5);
-$pdf->Cell(10,10,"Strasse Versender",0,0);
+$pdf->Cell(10,10,$strasse." ".$hausnummer,0,0);
 $pdf->Ln(5);
-$pdf->Cell(10,10,"Stadt Versender",0,0);
+$pdf->Cell(10,10,$plz." ".$ort,0,0);
 
 $pdf->Ln(10);
 $pdf->SetFont("times","",12);
@@ -15,9 +15,10 @@ $pdf->Cell(10,10,$empfaenger_nachname,0,0);
 $pdf->Ln(5);
 $pdf->Cell(10,10,$empfaenger_vorname,0,0);
 $pdf->Ln(5);
-$pdf->Cell(10,10,"Strasse Empfaenger",0,0);
+$pdf->Cell(10,10,$empfaenger_strasse." ".$empfaenger_hausnummer,0,0);
 $pdf->Ln(5);
-$pdf->Cell(10,10,"Stadt Empfaenger",0,0);
+$pdf->Cell(10,10,$empfaenger_plz." ".$empfaenger_ort,0,0);
 
 $pdf->Ln(5);
 $pdf->Cell(10,10,"________________________________________________________________________________",0,0);
+
