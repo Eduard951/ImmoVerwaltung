4ff577e97444d4d054a923f5d00ad5ed7557f2b6
Felix <Felix@10.7.201.64>
2019-06-12 16:19:51 +0200

    error handling

--- Software/ImmoVerwaltung/add_hausobjekt.php
+++ Software/ImmoVerwaltung/add_hausobjekt.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     require 'header.php';
     require 'includes/dbh.inc.php';
     if(isset($_SESSION['sessionid'])){
