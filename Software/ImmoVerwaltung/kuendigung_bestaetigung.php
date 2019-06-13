c06502df7e8f9d580ef332f2a3b96d2f69bce2f3
fewie <felix.wiebe@student.uni-siegen.de>
2019-06-13 21:39:03 +0200

    kündigungsbestätigung angepasst

--- Software/ImmoVerwaltung/kuendigung_bestaetigung.php
+++ Software/ImmoVerwaltung/kuendigung_bestaetigung.php
@@ -1,49 +1,49 @@
 <?php
-require "header.php";
-require "includes/dbh.inc.php";
-?>
 
+    require "header.php";
+    require "includes/dbh.inc.php";
+
+?>
 
 <main>
 	<?php 
-// Prepared statements erstellen
+// Mieterinformationen durch den Vermieter abfragen
 	if(isset($_SESSION['sessionid'])){
 	    
-	    $sql = "SELECT *
-                FROM benutzer
-                JOIN mietverhaeltnis
-                ON benutzer.BenutzerID = mietverhaeltnis.Mieter
-                OR benutzer.BenutzerID = mietverhaeltnis.Vermieter
-                ";
+	    $sql_mieter = "SELECT * FROM benutzer JOIN mietverhaeltnis ON benutzer.BenutzerID = mietverhaeltnis.Mieter WHERE Vermieter = ?";
+	    $stmt = mysqli_stmt_init($conn);
+	    
+	    if(!mysqli_stmt_prepare($stmt, $sql_mieter)){
+	        header("Location: ../kuendigung_bestaetigung.php?mieter_getID_error");
+	        exit();
+	    }else{
+	        $id = $_SESSION['sessionid'];
+	        mysqli_stmt_bind_param($stmt, "i", $id);
+	        mysqli_stmt_execute($stmt);
+	        $result_mieter = mysqli_stmt_get_result($stmt);
+	    }
 
-$result = $conn->query($sql);
-
-echo '
-		<h2>Kuendigungsbestaetigung senden<span class="badge badge-secondary"></span></h2>
+  echo '<h2>Kuendigungsbestaetigung senden<span class="badge badge-secondary"></span></h2>
         <br>
-        <form action="includes/kuendigung_bestaetigung.inc.php" method="post" target="_blank">
+        <form action="includes/kuendigung_bestaetigung.inc.php" method="post">
         <h5>Datum der Kuendigung:</h5>
-        <textarea class="form-control" rows="1" type="text" name="kuendigung"></textarea>
+        <input type="date" name="kuendigungs_datum"></textarea>
 	    <br>
-        <h5>Ende der Nutzung des Wohnmraums:</h5>
-        <textarea class="form-control" rows="1" type="text" name="nutzungsende"></textarea>
+        <h5>Ende der Nutzung des Wohnraums:</h5>
+        <input type="date" name="nutzungsende_datum"></textarea>
 		<br>
         <h4>Mieter auswaehlen:
-        <br>
+        <br>';
 
-';
-
-if(!empty($result)){
-    while($row = $result->fetch_assoc()){
-        echo '<br><input type="checkbox" name="empfaenger[]" value="'.$row['Name']," ", $row['Vorname'], " ", $row['Strasse'], " ", $row['Hausnr'], " ", $row['PLZ'], " ", $row['Ort'].'">'.$row['Name'],", ", $row['Vorname'], ": ", $row['Strasse'], " ", $row['Hausnr'], ", ", $row['PLZ'], " ", $row['Ort'].'<br>
-        ';
+    if(!empty($result_mieter)){
+        while($row = $result_mieter->fetch_assoc()){
+            echo '<br><input type="radio" id="'.$row['BenutzerID'].'" name="empfaenger[]" value="'.$row['Name']," ", $row['Vorname'], " ", $row['Strasse'], " ", $row['Hausnr'], " ", $row['PLZ'], " ", $row['Ort'], " ", $row['BenutzerID'].'">'.$row['Name'],", ", $row['Vorname'], ": ", $row['Strasse'], " ", $row['Hausnr'], ", ", $row['PLZ'], " ", $row['Ort'].'
+                  <br>';
+        }
     }
-}
-echo '<br><button class="btn btn-success btn-lg" type="submit" name="kuendigung_bestaetigt_submit">Versenden</button>
-            
-        </form>
-<br><br><br><a href="index.php"><button class="btn btn-primary btn-lg">Zurueck</button>
-';
+    echo '<br><button class="btn btn-success btn-lg" type="submit" name="kuendigung_bestaetigt_submit">Versenden</button>
+          </form>
+          <br><br><br><a href="index.php"><button class="btn btn-primary btn-lg">Zurueck</button>';
 	}else {
 	    echo'';
 	}
