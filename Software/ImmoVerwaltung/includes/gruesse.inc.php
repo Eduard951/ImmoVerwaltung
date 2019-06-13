daf569a3bce8d15346086202308e1c0c6b5fde95
Eduard Scherf <50400099+Eduard951@users.noreply.github.com>
2019-06-13 08:42:09 +0200

    Update gruesse.inc.php

--- Software/ImmoVerwaltung/includes/gruesse.inc.php
+++ Software/ImmoVerwaltung/includes/gruesse.inc.php
@@ -1,6 +1,8 @@
 <?php
 
 session_start();
+
+
 if(isset($_POST['gruesse_submit'])){
    
     
@@ -8,8 +10,11 @@ if(isset($_POST['gruesse_submit'])){
     
     
     $text = $_POST['text'];
-   
+
+    /* 
+    if(!empty($_POST['empfaenger'])){
     foreach( $_POST['empfaenger'] as $value) {
+        
         $pieces = explode(" ", $value);
         $empfaenger_vorname = $pieces[1];
         $empfaenger_nachname = $pieces[0];
@@ -17,7 +22,7 @@ if(isset($_POST['gruesse_submit'])){
         $empfaenger_ort = $pieces[3];
         $empfaenger_strasse = $pieces[4];
         $empfaenger_hausnummer = $pieces[5];
-    
+    */
         
     
     //gucken, ob variablen leer sind, wenn ja dann emptyfield error
@@ -26,11 +31,13 @@ if(isset($_POST['gruesse_submit'])){
         exit();
     }else{
         $sql = "SELECT * FROM benutzer WHERE BenutzerID=?;";
+        $sql_insert_message = "INSERT INTO nachrichten(SenderID,EmpfaengerID,Text,Datei) VALUES(?,?,?,?);";
         
         $stmt = mysqli_stmt_init($conn);
+        $stmt_insert_messages = mysqli_stmt_init($conn);
        
         if(!mysqli_stmt_prepare($stmt, $sql)){
-            header("Location: ../index.php?error=sqlerror");
+            header("Location: ../gruesse.php?error=sqlerror");
             exit();
         }else{
             
@@ -50,27 +57,69 @@ if(isset($_POST['gruesse_submit'])){
                 $hausnummer = $row['Hausnr'];
                 $ort = $row['Ort'];
                 $plz = $row['PLZ'];
-                         
-                require './pdf_templates/basic_pdf/pdf_start.php';     
-
-                require './pdf_templates/basic_pdf/pdf_header.php';
-                
-                require './pdf_templates/basic_pdf/pdf_icon.php';
-                
-                require './pdf_templates/basic_pdf/pdf_anrede.php';
-   
-                require './pdf_templates/basic_pdf/pdf_freitext.php'; 
-
-                require './pdf_templates/basic_pdf/pdf_mfg.php';
-
-                require './pdf_templates/basic_pdf/pdf_ende.php';
                     
+                if(!empty($_POST['empfaenger'])){
+                    foreach( $_POST['empfaenger'] as $value) {
+                        
+                        $pieces = explode(" ", $value);
+                        $empfaenger_vorname = $pieces[1];
+                        $empfaenger_nachname = $pieces[0];
+                        $empfaenger_plz = $pieces[2];
+                        $empfaenger_ort = $pieces[3];
+                        $empfaenger_strasse = $pieces[4];
+                        $empfaenger_hausnummer = $pieces[5];
+                        $empfaenger_id = $pieces[6];
                 
-            }else{
-                header("Location: ../gruesse.php?error=error2");
-                exit();
+                    
+                    require './pdf_templates/basic_pdf/pdf_start.php';
+                    
+                    require './pdf_templates/basic_pdf/pdf_header.php';
+                    
+                    require './pdf_templates/basic_pdf/pdf_icon.php';
+                    
+                    require './pdf_templates/basic_pdf/pdf_anrede.php';
+                    
+                    require './pdf_templates/basic_pdf/pdf_freitext.php';
+                    
+                    require './pdf_templates/basic_pdf/pdf_mfg.php';
+                    
+                   
+                    
+                    
+                        
+                        $stmt_test = $conn->prepare("INSERT INTO nachrichten(SenderID,EmpfaengerID,Text,Datei) VALUES(?,?,?,?)"); 
+                        
+                        $content = $pdf->Output("S");
+                        
+                        $gruese = "Gruesse";
+                        //$l=1001;
+                        
+                        mysqli_stmt_bind_param($stmt_test, "iiss", $id,$empfaenger_id,$gruese,$content);
+                        
+                        mysqli_stmt_execute($stmt_test);
+                        //require './pdf_templates/basic_pdf/pdf_ende.php';
+                        
+                        header("Location: ../gruesse.php?success");
+                    
+                    
+                    
+                    //require './pdf_templates/basic_pdf/pdf_ende.php';
+                
+                
+                //gruesse();
+                
             }
         }
+        
+        
     }
+    
+    
+    
     }
+    
+    
+    }
+
 }
+
