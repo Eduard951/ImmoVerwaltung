200ed57767a256c6655f84ed1370c12ecb1b3ff3
francis <francis@DESKTOP-K7MAT8C>
2019-06-13 11:22:09 +0200

    ok

--- Software/ImmoVerwaltung/includes/Datenschutzauskunft.inc.php
+++ Software/ImmoVerwaltung/includes/Datenschutzauskunft.inc.php
@@ -5,32 +5,35 @@ session_start();
 
 <?php
 
-$id=$_SESSION['sessionid'];
-$sql = "SELECT * FROM benutzer WHERE BenuzterID=?;";
-$stmt = mysqli_stmt_init($conn);
-if(!mysqli_stmt_prepare($stmt, $sql)){
-    header("Location: ../index.php?error=sqlerror");
-    exit();
-}else{
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-    
-    if($row=mysqli_fetch_assoc($result)){
-        $n=row[Name]; 
-        $v=row[Vorame]; 
-        $s=row[Strasse];
-        $h=row[Hausnr];
-        $p=row[PLZ];
-        $o=row[Ort];    
-    }
 
-if(isset($_POST["submit"]))
-echo '<h4> Name:</h4>'.$n.' <br>';
-echo '<h4> Vorname:</h4>'.$v.' <br>';
-echo '<h4> E-Mail:</h4> '. $_SESSION['sessionmail'].' <br>';
-echo '<h4> Adresse:</h4> '.$s.$h.$p.$o.'<br>';
-{
+
+if(isset($_POST["submit"])) {
     
+    $id=$_SESSION['sessionid'];
+    $sql = "SELECT * FROM benutzer WHERE BenuzterID=?;";
+    $stmt = mysqli_stmt_init($conn);
+    
+    if(!mysqli_stmt_prepare($stmt, $sql)){
+        header("Location: ../index.php?error=sqlerror");
+        exit();
+    }else{
+        mysqli_stmt_bind_param($stmt, "i", $id);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+        
+        if($row=mysqli_fetch_assoc($result)){
+            $n=row[Name];
+            $v=row[Vorame];
+            $s=row[Strasse];
+            $h=row[Hausnr];
+            $p=row[PLZ];
+            $o=row[Ort];
+        
+
+        }
+        echo "<h4> Name:</h4>".$n." <br>";
+        echo "<h4> Vorname:</h4>".$v." <br>";
+        echo "<h4> E-Mail:</h4> ". $_SESSION["sessionmail"]." <br>";
+        echo "<h4> Adresse:</h4> ".$s.$h.$p.$o."<br>";
 }
     } ?>
