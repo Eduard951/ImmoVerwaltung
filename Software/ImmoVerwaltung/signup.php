fe5c580aa4810515f72eebd4b230767f84d032fa
francis <francis@DESKTOP-K7MAT8C>
2019-06-13 13:41:50 +0200

    ok

--- Software/ImmoVerwaltung/signup.php
+++ Software/ImmoVerwaltung/signup.php
@@ -5,28 +5,28 @@
 
 	<main>
 	<div class=container>
-	<div class=row>	<div class="col-md-offset-8 col-md-4 "><h2>Registrierung<span class="badge badge-secondary"></span></h2></div></div>
-		<form action="includes/signup.inc.php" method="post" class="col-md-offset-4 col-md-8">
+	<div class=row>	<div class="col-md-offset-6 col-md-4 "><h2>Registrierung<span class="badge badge-secondary"></span></h2></div></div>
+		<form action="includes/signup.inc.php" method="post" class="row">
 			 
-			<input class="form-control " type="text" name="name" placeholder="Name.."> 
+			<input class="col-md-offset-2 col-md-5" type="text" name="name" placeholder="Name.."> 
 			<br>
-			<input class="form-control  " type="text" name="nachname" placeholder="Nachname..">
+			<input class="col-md-offset-2 col-md-5  " type="text" name="nachname" placeholder="Nachname..">
 			<br>
-			<input class="form-control " type="text" name="mail" placeholder="E-Mail...">
+			<input class="col-md-offset-2 col-md-5 " type="text" name="mail" placeholder="E-Mail...">
 			<br>
-			<input class="form-control" type="password" name="pwd" placeholder="Passwort...">
+			<input class="col-md-offset-2 col-md-5" type="password" name="pwd" placeholder="Passwort...">
 			<br>
-			<input class="form-control" type="password" name="pwd_repeat" placeholder="Wiederhole passwort...">
+			<input class="col-md-offset-2 col-md-5" type="password" name="pwd_repeat" placeholder="Wiederhole passwort...">
 			<br>
-			<input class="form-control" type="text" name="strasse" placeholder="Strasse..">
+			<input class="col-md-offset-2 col-md-5" type="text" name="strasse" placeholder="Strasse..">
 			<br>
-			<input class="form-control" type="text" name="hausnr" placeholder="Hausnummer..">
+			<input class="col-md-offset-2 col-md-5" type="text" name="hausnr" placeholder="Hausnummer..">
 			<br>
-			<input class="form-control" type="text" name="plz" placeholder="PLZ..">
+			<input class="col-md-offset-2 col-md-5" type="text" name="plz" placeholder="PLZ..">
 			<br>
-			<input class="form-control" type="text" name="ort" placeholder="Ort..">
+			<input class="col-md-offset-2 col-md-5" type="text" name="ort" placeholder="Ort..">
 			<br>
-			<button class="btn btn-secondary btn-lg form-control" type="submit" name="signup_submit">Registrieren</button>
+			<button class="btn btn-secondary btn-lg col-md-offset-2 col-md-5" type="submit" name="signup_submit">Registrieren</button>
 		</form>
 		</div>
 	</main>
