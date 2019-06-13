2c055783ca443388e88c0e89af709eae96ef083c
francis <francis@DESKTOP-K7MAT8C>
2019-06-13 12:59:10 +0200

    ok

--- Software/ImmoVerwaltung/includes/Datenschutzauskunft.inc.php
+++ Software/ImmoVerwaltung/includes/Datenschutzauskunft.inc.php
@@ -12,15 +12,9 @@ if(isset($_POST["submi"])) {
     $id=$_SESSION['sessionid'];
    $m= $_SESSION['sessionmail'];
     
-    $query=mysqli_query($conn," select * from benutzer WHERE Email=' $m'");
+   
     
-    while ($row=mysqli_fetch_array($query ) )
-    {
-        $H= $row["Name"]    ;
-        
-    }
-    
-  /*  $sql = "SELECT * FROM benutzer WHERE BenuzterID=?;";
+    $sql = "SELECT * FROM benutzer WHERE BenuzterID=?;";
     $stmt = mysqli_stmt_init($conn);
     
     if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -32,29 +26,26 @@ if(isset($_POST["submi"])) {
         $result = mysqli_stmt_get_result($stmt);
         
         if($row=mysqli_fetch_assoc($result)){
-            $n=$row["Name"];
-            $v=$row["Vorame"];
-            $s=$row["Strasse"];
-            $h=$row["Hausnr"];
-            $p=$row["PLZ"];
-            $o=$row["Ort"];  
+            $n=$row['Name'];
+            $v=$row['Vorame'];
+            $s=$row['Strasse'];
+            $h=$row['Hausnr'];
+            $p=$row['PLZ'];
+            $o=$row['Ort'];  
          
 
         }
-        */
+        
        /* echo "<h4> Name:</h4>".$n." <br>";
         echo "<h4> Vorname:</h4>".$v." <br>";
         echo "<h4> E-Mail:</h4> ". $_SESSION["sessionmail"]." <br>";
    echo "<h4> Adresse:</h4> ".$s.$h.$p.$o."<br>";*/
     
-    echo "meine";
-    echo" $id";
-    echo "$m";
-    echo "$H";
+   
 } echo "meine";
     echo" $id";
     echo "$m";
     echo "$H";
-//}
+}
      
     ?>
