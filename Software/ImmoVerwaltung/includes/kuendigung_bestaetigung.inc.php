d31d78b5eb5cf07c3435c9e1df90c512e5bf2009
fewie <50362715+fewie@users.noreply.github.com>
2019-06-13 21:39:59 +0200

    Kündigungsbestätigung aktualisiert

--- Software/ImmoVerwaltung/includes/kuendigung_bestaetigung.inc.php
+++ Software/ImmoVerwaltung/includes/kuendigung_bestaetigung.inc.php
@@ -6,8 +6,8 @@ if(isset($_POST['kuendigung_bestaetigt_submit'])){
     require 'dbh.inc.php';
     require "../lib/fpdf181/fpdf.php";
     
-    $datum_kuendigung = $_POST['kuendigung'];
-    $datum_nutzungsende = $_POST['nutzungsende'];
+    $datum_kuendigung = $_POST['kuendigungs_datum'];
+    $datum_nutzungsende = $_POST['nutzungsende_datum'];
       
     foreach( $_POST['empfaenger'] as $value) {
         $pieces = explode(" ", $value);
@@ -17,12 +17,8 @@ if(isset($_POST['kuendigung_bestaetigt_submit'])){
         $empfaenger_hausnr = $pieces[3];
         $empfaenger_plz = $pieces[4];
         $empfaenger_ort = $pieces[5];
-        
-        //gucken, ob variablen leer sind, wenn ja dann emptyfield error
-        if(empty($datum_kuendigung) || empty($datum_nutzungsende)){
-            header("Location: ../letter.php?error=emptyfields");
-            exit();
-        }else{
+        $empfaenger_id = $pieces[6];
+       
             $sql = "SELECT * FROM benutzer WHERE BenutzerID=?;";
             $stmt = mysqli_stmt_init($conn);
             if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -60,24 +56,40 @@ if(isset($_POST['kuendigung_bestaetigt_submit'])){
                     $pdf->Ln(36);
                     $pdf->SetFont("times","B",9);
                     $pdf->Cell(14);
-                    $pdf->Cell(10,10,"Ihre Künndigung des Mietverhältnisses zum $datum_nutzungsende",0,0);
+                    $pdf->Cell(10,10,"Ihre Kuendigung des Mietverhaeltnisses zum $datum_nutzungsende",0,0);
                     $pdf->Ln(20);
                     $pdf->SetFont("times","",9);
                     $pdf->Cell(14);
                     $pdf->Cell(10,10,"Sehr geehrte/r Frau/Herr $empfaenger_nachname,",0,0);
                     $pdf->Ln(12);
                     $pdf->Cell(14);
-                    $pdf->MultiCell(0,4,"hiermit bestätige ich Ihre Kündigung des Wohnraummietvertrages der Wohnung in $empfaenger_strasse $empfaenger_hausnr, $empfaenger_plz $empfaenger_ort vom $datum_kuendigung fristgerecht zum $datum_nutzungsende.", 0,"L",false);
+                    $pdf->MultiCell(0,4,"hiermit bestaetige ich Ihre Kuendigung des Wohnraummietvertrages der Wohnung in $empfaenger_strasse $empfaenger_hausnr, $empfaenger_plz $empfaenger_ort vom $datum_kuendigung fristgerecht zum $datum_nutzungsende.", 0,"L",false);
                     $pdf->Ln(4);
                     $pdf->Cell(14);
-                    $pdf->MultiCell(0,4,"Ich bitte Sie sich nach dem Auszug zu melden, damit bei einer Begehung ein abschließendes Übergabeprotokoll erstellt und die Schlüsselübergabe geregelt werden kann.", 0,"L",false);
+                    $pdf->MultiCell(0,4,"Ich bitte Sie sich nach dem Auszug zu melden, damit bei einer Begehung ein abschliessendes Uebergabeprotokoll erstellt und die Schluesseluebergabe geregelt werden kann.", 0,"L",false);
                     $pdf->Ln(12);
                     $pdf->Cell(14);
-                    $pdf->Cell(10,10,"Mit freundlichen Grüßen,",0,0);
+                    $pdf->Cell(10,10,"Mit freundlichen Gruessen,",0,0);
                     $pdf->Ln(4);
                     $pdf->Cell(14);
                     $pdf->Cell(10,10,"$sender_vorname $sender_nachname",0,0);
-                    $pdf->Output();
+
+                    //PDF an Mieter verschicken   
+                    $sql_msg = "INSERT INTO nachrichten(SenderID, EmpfaengerID, Text, Datei) VALUES(?, ?, ?, ?)";
+                    
+                    if(mysqli_stmt_prepare($stmt, $sql_msg)){
+                        
+                        $content = $pdf->Output("S");
+                        $kb = "Kuendigungsbestaetigung Vermieter"." ".$sender_nachname;
+
+                        mysqli_stmt_bind_param($stmt, "iiss", $id, $empfaenger_id, $kb, $content);
+                        mysqli_stmt_execute($stmt);
+                      
+                        header("Location: ../kuendigung_bestaetigung.php?success");
+                    }else{
+                        header("Location: ../kuendigung_bestaetigung.php?errormsg");
+                        echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
+                    }
                           
                 }else{
                     header("Location: ../kuendigung_bestaetigung.php?error=error");
@@ -86,4 +98,3 @@ if(isset($_POST['kuendigung_bestaetigt_submit'])){
             }
         }
     }
-}
