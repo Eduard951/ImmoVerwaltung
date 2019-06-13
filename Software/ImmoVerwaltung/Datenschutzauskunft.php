147af08e455172d1a642a9267f745d04aa4a0aa9
francis <francis@DESKTOP-K7MAT8C>
2019-06-13 22:23:27 +0200

    ok

--- Software/ImmoVerwaltung/Datenschutzauskunft.php
+++ Software/ImmoVerwaltung/Datenschutzauskunft.php
@@ -6,16 +6,55 @@ require "includes/dbh.inc.php";
 
 ?>
 <?php  
+session_start();
 if(isset($_SESSION['sessionid'])){
    
 
-    echo' <form action="includes/Datenschutzauskunft.inc.php" method="POST">
+    echo' <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
 <h3> Meine Daten Ansehen</h3></br>
-<button type="submit" class="btn btn-success" name="submi "> suchen</button>
-
+<button type="submit" class="btn btn-success" name="submi "> </button>';
+echo'';
      
 
-</form> ';
+
+$sql = "SELECT * FROM benutzer WHERE BenutzerID=?;";
+
+$stmt = mysqli_stmt_init($conn);
+
+
+if(!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: ../index.php?error=sqlerror");
+    exit();
+}else{
+    
+    $id = $_SESSION['sessionid'];
+    
+    mysqli_stmt_bind_param($stmt, "i", $id);
+    
+    mysqli_stmt_execute($stmt);
+    
+    $result = mysqli_stmt_get_result($stmt);
+    
+    if($row=mysqli_fetch_assoc($result)){
+        
+        $vorname = $row['Vorname'];
+        $nachname = $row['Name'];
+        $strasse = $row['Strasse'];
+        $ort = $row['Ort'];
+        $plz = $row['PLZ'];
+        $hausnummer = $row['Hausnr'];
+    }
+    echo "$vorname";
+    echo "$nachname";
+    echo "$strasse";
+    echo "$ort";
+    echo "$plz";
+    echo "$hausnummer";
+    echo "oui";
+     
+}
+
+echo'</form>';
     
 }
 ?>
